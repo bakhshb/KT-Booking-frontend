2f62f7f47a0b1e06bf1f19fa0109fc1f64e9fde4
Rowan <rowanovenden@gmail.com>
2014-10-31 09:04:12 +1300

    Added gallery plugin

--- app/gallery/gallery.js
+++ app/gallery/gallery.js
@@ -13,6 +13,42 @@ angular.module('ktApp.gallery', [])
 
 .controller('GalleryCtrl', ['$scope', function($scope) {
 
-	
+	$scope.testitems = [{
+        src: 'assets/img/logo.jpg'
+      },
+	  {
+        src: 'assets/img/banner.jpg'
+      }];
 
-}]);
+}])
+
+
+.directive('gallery', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+			scope.$watch(attrs.test, function(value){
+                console.log(value);
+            });
+            $(element).magnificPopup({
+				//delegate: 'a', //todo: currently adding on scope, or do by html tag?
+				items: scope.$eval(attrs.test),
+				gallery: {
+					enabled: true,
+					navigateByImgClick: true,
+					preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+				},
+				type: 'image',
+				/*TODO
+				tLoading: 'Loading image #%curr%...',
+				image: {
+					tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+					titleSrc: function(item) {
+						return item.el.attr('title') + '<small>qweqwe</small>';
+					}
+				}
+				*/
+			});
+        }
+    };
+});
